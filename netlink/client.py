6cb34d87ec982825a59ea8990217b56c1c7f36b9
Oliver Wright <meiamsome@meiamso.me>
2015-03-30 20:47:09 +0100

    Added net services
    Added client and server modules to be used to implement basic control systems.

---
+++ netlink/client.py
@@ -0,0 +1,227 @@
+from __future__ import print_function
+
+import sys
+import json
+import socket
+import common
+import threading
+import errno
+from datetime import datetime, timedelta
+import time
+import collections
+
+class Client(object):
+    def __init__(self, udp_port=23457):
+        self.udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.udp_sock.bind(('', udp_port))
+        self.udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.udp_sock.settimeout(0)
+        # Set broadcast
+        self.udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.going = False
+        self.servers = []
+        self.server_list = {}
+        self.server_update_time = datetime.now()
+        self.server_udp_port = 23456
+        self.main_thread = None
+
+    def start(self):
+        if self.going:
+            return
+        if self.main_thread:
+            self.main_thread.join()
+        self.going = True
+        self.my_thread = threading.Thread(None, Client._main_client_thread, "Main client thread", [self])
+        self.my_thread.start()
+
+    def stop(self, close_servers=True):
+        if not self.going:
+            return
+        self.going = False
+        self.my_thread.join()
+        if close_servers:
+            for x in self.servers:
+                x.disconnect()
+
+    def last_update(self):
+        return self.server_update_time
+
+    def _main_client_thread(self):
+        try:
+            my_last_broadcast = datetime.now() - timedelta(minutes=30)
+            while self.going:
+                # Check for server broadcasts
+                try:
+                    data, other = self.udp_sock.recvfrom(1024)
+                    if data[0] == common._BROADCAST_IP: # Server broadcasting it's presence
+                        server_data = json.loads(data[1:])
+                        for address in server_data[1]:
+                            if address in self.server_list:
+                                self.server_list[address].seen()
+                                break
+                        else:
+                            self.server_update_time = datetime.now()
+                            server = Server(server_data[0], other[0], server_data[2], server_data[1])
+                            for address in server_data[1]:
+                                self.server_list[address] = server
+                            self.server_list[other[0]] = server
+                            self.servers.append(server)
+                    else:
+                        print(data, file=sys.stderr)
+                except socket.error as error:
+                    if error.errno != errno.EWOULDBLOCK:
+                        raise
+                # Query servers
+                if datetime.now() - my_last_broadcast > timedelta(seconds=5):
+                    self.udp_sock.sendto(common._BROADCAST_REQUEST, ('<broadcast>', self.server_udp_port))
+                    my_last_broadcast = datetime.now()
+
+                # Check for dead servers
+                dead = [server for server in self.servers if server.has_expired()]
+                for server in dead:
+                    for address in server.addresses:
+                        del self.server_list[address]
+                    del self.server_list[server.address]
+                    self.servers.remove(server)
+                    self.server_update_time = datetime.now()
+
+                # Sleepy time
+                time.sleep(1./20)
+        finally:
+            self.going = False
+
+class Server(object):
+    def __init__(self, name, address, ports, addresses):
+        self.name = name
+        self.address = address
+        self.ports = ports
+        self.addresses = addresses
+        self.last_seen = datetime.now()
+        self.connected = False
+        self.sock = None
+        self.thread = None
+        # Queues
+        self.recv_queues = {}
+        self.out_buffer = ""
+
+    def seen(self):
+        self.last_seen = datetime.now()
+
+    def has_expired(self):
+        if self.connected:
+            self.seen()
+        return datetime.now() - self.last_seen > timedelta(minutes=1)
+
+    def connect(self):
+        if self.connected:
+            return
+        if self.thread:
+            self.thread.join()
+        self.connected = True
+        self.thread = threading.Thread(None, Server._connection_thread, "Server connection", [self])
+        self.thread.start()
+
+    def is_connected(self):
+        return self.connected
+
+    def disconnect(self):
+        if not self.connected:
+            return
+        self.connected = False
+        self.thread.join()
+
+
+    def stream_generator(self, stream_id=1):
+        if stream_id < 1 or stream_id > 255:
+            raise ValueError("Stream id is invalid")
+        if stream_id in self.recv_queues:
+            raise ValueError("A generator for this stream is already open")
+        self.recv_queues[stream_id] = collections.deque()
+        return self._stream_generator(stream_id)
+
+    def _stream_generator(self, stream_id):
+        my_queue = self.recv_queues[stream_id]
+        while True:
+            value = None
+            try:
+                value = my_queue.popleft()
+            except IndexError:
+                pass
+            if (yield value) is not None:
+                break
+        del self.recv_queues[stream_id]
+
+    def send_to_stream(self, message, stream_id=1):
+        if stream_id < 1 or stream_id > 255:
+            raise ValueError("Stream id is invalid")
+        if chr(0) in message:
+            raise ValueError("There is no support for null characters on the stream.")
+        if not self.connected:
+            raise Exception("The socket is closed.")
+        self.out_buffer += chr(stream_id) + message + chr(0)
+
+    def _connection_thread(self):
+        # TODO: Connect using alternate addresses if possible.
+        try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.connect((self.address, self.ports[1]))
+            self.sock.settimeout(0.1)
+            in_buffer = ""
+            self.out_buffer = ""
+            while self.connected:
+                try:
+                    message = self.sock.recv(1024)
+                    if not message:
+                        break
+                    in_buffer += message
+                except socket.timeout:
+                    pass
+                except socket.error as e:
+                    if e.errno == errno.EWOULDBLOCK:
+                        pass
+                    else:
+                        raise
+                while chr(0) in in_buffer:
+                    message, in_buffer = in_buffer.split(chr(0), 1)
+                    mess_id = ord(message[0])
+                    if mess_id in self.recv_queues:
+                        self.recv_queues[mess_id].append((self, message[1:]))
+                if self.out_buffer:
+                    sent = self.sock.send(self.out_buffer)
+                    self.out_buffer = self.out_buffer[sent:]
+        finally:
+            self.connected = False
+            self.sock.close()
+
+
+if __name__ == '__main__':
+    client = Client()
+    update = client.last_update()
+    try:
+        client.start()
+        loop_count = 0
+        iterators = []
+        while True:
+            time.sleep(.1)
+            if update < client.last_update():
+                update = client.last_update()
+                print([(x.name, x.last_seen) for x in client.servers])
+                for x in client.servers:
+                    x.connect()
+                    for sid in xrange(1, 11):
+                        iterators.append(x.stream_generator(stream_id=sid))
+            loop_count += 1
+            if not loop_count % 10:
+                for server in client.servers:
+                    if server.is_connected():
+                        for x in xrange(1, 11):
+                            server.send_to_stream("Testing Channel " + str(x), stream_id=x)
+                    else:
+                        server.connect()
+            for iterate in iterators:
+                for x in iterate:
+                    if x is None:
+                        break
+                    print(x)
+    finally:
+        client.stop()
---
+++ netlink/server.py
@@ -0,0 +1,177 @@
+import socket
+import threading
+import collections
+import time
+import json
+from datetime import datetime, timedelta
+import common
+import errno
+
+class Server(object):
+    debug = True
+
+    def __init__(self, server_name=socket.gethostname(), udp_port=23456, tcp_port=23456, client_udp=23457):
+        self.going = False
+        self.server_name = server_name
+        self.udp_port = udp_port
+        self.tcp_port = tcp_port
+        self.client_udp_port = client_udp
+        # Make our UDP socket
+        self.udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.udp_sock.bind(('', self.udp_port))
+        self.udp_sock.setblocking(False)
+        # Set broadcast
+        self.udp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        # Set up our listening TCP
+        self.tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.tcp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.tcp_sock.bind(('', self.tcp_port))
+        self.tcp_sock.listen(64)
+        self.tcp_sock.settimeout(0)
+        self.main_thread = None
+        # Set up our queues
+        self.recv_queues = {}
+        # Client Storage
+        self.clients = []
+        super(Server, self).__init__()
+
+    def start(self):
+        if self.going:
+            return
+        self.going = True
+        if self.main_thread:
+            self.main_thread.join()
+        self.main_thread = threading.Thread(None, Server._main_server_thread, "Main server thread", [self])
+        self.main_thread.start()
+
+    def stop(self):
+        if not self.going:
+            return
+        self.going = False
+        self.main_thread.join()
+
+    def stream_generator(self, stream_id=1):
+        if stream_id < 1 or stream_id > 255:
+            raise ValueError("Stream id is invalid")
+        if stream_id in self.recv_queues:
+            raise ValueError("A generator for this stream is already open")
+        self.recv_queues[stream_id] = collections.deque()
+        return self._stream_generator(stream_id)
+
+    def _stream_generator(self, stream_id):
+        my_queue = self.recv_queues[stream_id]
+        while True:
+            value = None
+            try:
+                value = my_queue.popleft()
+            except IndexError:
+                pass
+            if (yield value) is not None:
+                break
+        del self.recv_queues[stream_id]
+
+    def send_to_stream(self, message, stream_id=1, client_id=None):
+        if stream_id < 1 or stream_id > 255:
+            raise ValueError("Stream id is invalid")
+        if chr(0) in message:
+            raise ValueError("There is no support for null characters on the stream.")
+        if client_id is not None:
+            if self.clients[client_id] is not None:
+                self.clients[client_id][2].append((stream_id, message))
+        else:
+            for client in self.clients:
+                if client is not None:
+                    client[2].append((stream_id, message))
+
+    def _main_server_thread(self):
+        try:
+            if threading.current_thread() != self.main_thread:
+                raise Exception("Don't call this like this!")
+            last_broadcast_time = datetime.now() - timedelta(minutes=30)
+            info = common._BROADCAST_IP + json.dumps([self.server_name, socket.gethostbyname_ex(socket.gethostname())[2], (self.udp_port, self.tcp_port)])
+            while self.going:
+                try:
+                    # Try the UDP socket
+                    data, other = self.udp_sock.recvfrom(1024)
+                    if data[0] == common._BROADCAST_REQUEST:
+                        self.udp_sock.sendto(info, other)
+                    elif Server.debug:
+                        print("Received data: ", data)
+                except socket.error as err:
+                    if err.errno != errno.EWOULDBLOCK:
+                        raise err
+                # Read the TCP stream
+                try:
+                    sock, address = self.tcp_sock.accept()
+                    the_thread = threading.Thread(None, Server._client_server_thread, "Server client thread", [self, sock, len(self.clients)])
+                    self.clients.append((address, the_thread, collections.deque()))
+                    the_thread.start()
+                except socket.error as err:
+                    if err.errno != errno.EWOULDBLOCK:
+                        raise
+                # Send the server query
+                if datetime.now() - last_broadcast_time > timedelta(seconds=5):
+                    self.udp_sock.sendto(info, ('<broadcast>', self.client_udp_port))
+                    last_broadcast_time = datetime.now()
+                time.sleep(1./20)
+        finally:
+            self.going = False
+
+    def _client_server_thread(self, client, client_number):
+        client.settimeout(0.1)
+        in_buffer = ""
+        out_buffer = ""
+        while self.going:
+            # Handle incoming message
+            try:
+                message = client.recv(1024)
+                if not message:
+                    # Our client has disconnected.
+                    break
+                in_buffer += message
+            except socket.timeout as err:
+                pass
+            except socket.error as err:
+                if err.errno != errno.EWOULDBLOCK:
+                    raise
+            while chr(0) in in_buffer:
+                message, in_buffer = in_buffer.split(chr(0), 1)
+                mess_id = ord(message[0])
+                if mess_id in self.recv_queues:
+                    self.recv_queues[mess_id].append((client_number, message[1:]))
+
+            # Post all outgoing
+            while True:
+                try:
+                    message = self.clients[client_number][2].popleft()
+                except IndexError:
+                    break
+                out_buffer += chr(message[0]) + message[1] + chr(0)
+            if out_buffer:
+                sent_count = client.send(out_buffer)
+                out_buffer = out_buffer[sent_count:]
+        self.clients[client_number] = None
+        client.close()
+
+if __name__ == "__main__":
+    my_server = Server()
+    sid = 10 # Stream ID to echo on
+    it = None
+    try:
+        my_server.start()
+        it = my_server.stream_generator(stream_id=sid)
+        for x in it:
+            if x is None:
+                time.sleep(0.5)
+            else:
+                print(x)
+                my_server.send_to_stream(x[1], stream_id=sid, client_id=x[0])
+    finally:
+        if it is not None:
+            # This is the correct way to stop a queue.
+            try:
+                it.send(0)
+            except StopIteration:
+                pass
+        my_server.stop()
